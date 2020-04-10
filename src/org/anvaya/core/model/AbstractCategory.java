a732f9aaacde92596a33cecdd2d097cdfa0f8bb5
Andrei Gache <andrei.gache.99@gmail.com>
2020-04-10 12:52:53 +0200

    first up

---
+++ src/org/anvaya/core/model/AbstractCategory.java
@@ -0,0 +1,13 @@
+/*
+ * Created by IntelliJ IDEA.
+ * Package Name: org.anvaya.core.model
+ * User: Andrei Gache.
+ * Email: andrei.gache.99@gmail.com
+ * Website: https://andrei-gache.com
+ * Date: 10/04/20
+ * Time: 12:47
+ */
+package org.anvaya.core.model;
+
+public abstract class AbstractCategory {
+}
---
+++ src/org/anvaya/core/request/AbstractRequest.java
@@ -0,0 +1,13 @@
+/*
+ * Created by IntelliJ IDEA.
+ * Package Name: org.anvaya.core.request
+ * User: Andrei Gache.
+ * Email: andrei.gache.99@gmail.com
+ * Website: https://andrei-gache.com
+ * Date: 10/04/20
+ * Time: 12:49
+ */
+package org.anvaya.core.request;
+
+public abstract class AbstractRequest implements Request{
+}
